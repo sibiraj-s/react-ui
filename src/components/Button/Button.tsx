898c82053872125e6513a998c21f58efc052beb8
Sibiraj <dev.sibiraj@outlook.com>
2022-08-09 19:50:32 +0530

    Add react-aria for aria props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,6 @@
 import { ElementType, forwardRef, ReactElement } from 'react';
+import { useButton } from 'react-aria';
+import { useObjectRef, mergeProps } from '@react-aria/utils';
 
 import { styled, VariantProps, CSS } from '../../stitches.config';
 import { PolymorphicComponentPropWithRef, PolymorphicRef } from '../../utils/types';
@@ -64,7 +66,10 @@ type ButtonOwnProps = ButtonVariants & ButtonExtraProps;
 type ButtonProps<T extends ElementType> = PolymorphicComponentPropWithRef<T, ButtonOwnProps>;
 
 export const BaseButton = <T extends ElementType = 'button'>(props: ButtonProps<T>, ref: PolymorphicRef<T>) => {
-  return <StyledButton {...props} ref={ref} />;
+  const buttonRef = useObjectRef<PolymorphicRef<T>>(ref);
+  const { buttonProps } = useButton(props, buttonRef);
+
+  return <StyledButton {...mergeProps(buttonProps, props)} ref={buttonRef} />;
 };
 
 type ButtonComponent = <T extends ElementType = 'button'>(props: ButtonProps<T>) => ReactElement<T>;
