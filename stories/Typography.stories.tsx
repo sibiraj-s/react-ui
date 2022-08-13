b05266dad6a00962e1250a0d0ed91a2680644ae8
Sibiraj <dev.sibiraj@outlook.com>
2022-08-13 22:08:10 +0530

    Add Typograhpy stories

--- stories/Typography.stories.tsx
+++ stories/Typography.stories.tsx
@@ -4,7 +4,7 @@ import { Fragment } from 'react';
 import { Heading, Text } from '../index';
 
 export default {
-  title: 'Typography/Heading',
+  title: 'Typography/Typography',
   component: Heading,
   argTypes: {},
   subcomponents: {
@@ -20,32 +20,16 @@ const Template: ComponentStory<typeof Heading> = () => (
     <Heading size='4'>h4: This is a heading level 4</Heading>
     <Heading size='5'>h5: This is a heading level 5</Heading>
     <Heading size='6'>h6: This is a heading level 6</Heading>
-  </Fragment>
-);
-
-const VariantsTemplate = () => (
-  <Fragment>
-    <Heading size='1' variant='primary'>
-      h1: This is a primary variant heading level 1
-    </Heading>
-    <Heading size='2' variant='secondary'>
-      h2: This is a secondary variant heading level 2
-    </Heading>
-    <Heading size='3' variant='success'>
-      h3: This is a success variant heading level 3
-    </Heading>
-    <Heading size='4' variant='danger'>
-      h4: This is a danger variant heading level 4
-    </Heading>
-    <Heading size='5' variant='muted'>
-      h5: This is a muted variant heading level 5
-    </Heading>
-    <Heading size='6'>h6: This is a no variant heading level 6</Heading>
+    <Text>
+      p: This is a Paragraph size 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+      deserunt mollit anim id est laborum.
+    </Text>
   </Fragment>
 );
 
 export const Default = Template.bind({});
 Default.args = {};
-
-export const Variants = VariantsTemplate.bind({});
-Default.args = {};
+Default.storyName = 'Typography';
