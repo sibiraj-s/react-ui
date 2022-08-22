fdad99198b0d40b2b5cd2af440a88ebabbc171ba
Sibiraj <dev.sibiraj@outlook.com>
2022-08-22 21:09:35 +0530

    Add help/error text to TextInputField

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -2,21 +2,28 @@ import { ComponentProps, ElementRef, forwardRef } from 'react';
 import { AriaTextFieldProps, useTextField } from 'react-aria';
 import { useObjectRef } from '@react-aria/utils';
 
-import Input from '../Input';
 import FormControl, { FormControlOwnProps } from '../FormControl';
-import Label from '../Label/Label';
+import Label from '../Label';
+import Input from '../Input';
+import FormText from '../FormText';
 
 type TextFieldOwnProps = ComponentProps<typeof Input>;
 type TextFieldProps = FormControlOwnProps & TextFieldOwnProps & AriaTextFieldProps;
 
 export const TextField = forwardRef<ElementRef<typeof Input>, TextFieldProps>((props, ref) => {
   const inputRef = useObjectRef(ref);
-  const { labelProps, inputProps } = useTextField(props, inputRef);
+  const { labelProps, inputProps, descriptionProps, errorMessageProps } = useTextField(props, inputRef);
 
   return (
     <FormControl isInvalid={props.isInvalid}>
       <Label {...labelProps}>{props.label}</Label>
       <Input {...inputProps} ref={inputRef} />
+      {props.description && <FormText {...descriptionProps}>{props.description}</FormText>}
+      {props.errorMessage && (
+        <FormText variant='danger' {...errorMessageProps}>
+          {props.errorMessage}
+        </FormText>
+      )}
     </FormControl>
   );
 });
--- stories/TextField.stories.tsx
+++ stories/TextField.stories.tsx
@@ -16,9 +16,17 @@ Default.args = {
   placeholder: 'Type something...',
 };
 
+export const WithFormText = Template.bind({});
+WithFormText.args = {
+  label: 'Enter your name',
+  placeholder: 'Type something...',
+  description: 'pretium lectus quam id leo in vitae turpis massa sed',
+};
+
 export const Invalid = Template.bind({});
 Invalid.args = {
   label: 'Enter your name',
   placeholder: 'Type something...',
   isInvalid: true,
+  errorMessage: 'pretium lectus quam id leo in vitae turpis massa sed',
 };
