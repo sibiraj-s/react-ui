a88a5c38fcda8ede3427111dafbab3e3c67f84da
Sibiraj <dev.sibiraj@outlook.com>
2022-08-22 21:26:53 +0530

    Add invalid Textarea variant

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,6 +1,7 @@
 import { ComponentProps, ElementRef, forwardRef } from 'react';
 
 import { styled, VariantProps, CSS } from '../../stitches.config';
+import { useFormControlContext } from '../FormControl';
 import { StyledInput } from '../Input';
 
 export const StyledTextarea = styled(StyledInput, {
@@ -14,7 +15,10 @@ type TextareaOwnProps = TextareaVariants & TextareaExtraProps;
 type TextareaProps = TextareaOwnProps & ComponentProps<'textarea'>;
 
 export const Textarea = forwardRef<ElementRef<'textarea'>, TextareaProps>((props, ref) => {
-  return <StyledTextarea as='textarea' {...props} ref={ref} />;
+  const fromContol = useFormControlContext();
+  const isInvalid = props.isInvalid ?? fromContol.isInvalid ?? false;
+
+  return <StyledTextarea as='textarea' {...props} isInvalid={isInvalid} ref={ref} />;
 });
 
 Textarea.displayName = 'Textarea';
