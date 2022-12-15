846f97332d9c3c17bf738268811d45b7539fedcd
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2022-12-15 17:03:22 +0530

    Redundant storySort function

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -21,7 +21,4 @@ export const parameters = {
     },
   },
   backgrounds: { default: 'light' },
-  options: {
-    // storySort: (a, b) => (a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })),
-  },
 };
