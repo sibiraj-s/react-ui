4d8da177406c5066fe018262b9b5b89ff2d11c7f
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-08-10 14:01:31 +0530

    Add shadow variants

--- src/components/AlertDialog/AlertDialogContent.tsx
+++ src/components/AlertDialog/AlertDialogContent.tsx
@@ -16,7 +16,7 @@ const StyledAlertDialogContent = styled(AlertDialogPrimitive.Content, {
   zIndex: 100,
   backgroundColor: 'white',
   borderRadius: '$roundedSm',
-  boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
+  boxShadow: '$lg',
   position: 'fixed',
   width: '90vw',
   maxWidth: '32rem',
