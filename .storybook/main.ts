ba1f74b15c8dbd02ad67279c403d3d1e727b6711
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-08-15 08:10:26 +0530

    Remove storybook addon interactions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
     name: '@storybook/react-vite',
     options: {
