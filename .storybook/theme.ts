1e6ff3b8c828439e1e8b46162f63eb742027c15e
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-10-22 13:25:21 +0530

    Update storybook font family

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -5,4 +5,6 @@ export default create({
   brandTitle: 'React UI',
   brandUrl: 'https://github.com/sibiraj-s/react-ui',
   brandTarget: '_blank',
+  fontBase:
+    'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
 });
