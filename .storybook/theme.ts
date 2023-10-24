63f82cd29a62f51608a1405534c51fad7cd10919
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-10-24 14:30:47 +0530

    Use correct font for storybook

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -1,10 +1,10 @@
 import { create } from '@storybook/theming';
+import { token } from '../styled-system/tokens';
 
 export default create({
   base: 'light',
   brandTitle: 'React UI',
   brandUrl: 'https://github.com/sibiraj-s/react-ui',
   brandTarget: '_blank',
-  fontBase:
-    'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+  fontBase: token('fonts.sans'),
 });
