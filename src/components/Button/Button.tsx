0b0dd1e065ba2a2dde08321a5f956662fdc88e64
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-11-16 15:42:40 +0530

    Cleanup Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,37 +1,13 @@
-import { ComponentProps, ElementRef, forwardRef } from 'react';
-import { AriaButtonProps, useButton } from '@react-aria/button';
-import { splitCssProps, styled } from 'styled-system/jsx';
-import { buttonRecipe, ButtonRecipeVariantProps } from 'styled-system/recipes';
+import { styled } from 'styled-system/jsx';
+import { buttonRecipe } from 'styled-system/recipes';
 
-import useObjectRef from '@/hooks/use-object-ref';
 import rx from '@/utils/factory';
 
-const StyledButton = styled(rx.button, buttonRecipe);
-type ButtonOwnProps = ComponentProps<typeof StyledButton> & ButtonRecipeVariantProps;
-
-type ButtonElement = ElementRef<typeof StyledButton>;
-type ButtonProps = ButtonOwnProps & AriaButtonProps;
-
-export const Button = forwardRef<ButtonElement, ButtonProps>((props, forwardedRef) => {
-  const buttonRef = useObjectRef(forwardedRef);
-  const [variantProps, { children, asChild, ...restProps }] = buttonRecipe.splitVariantProps(props);
-  const [cssProps] = splitCssProps(restProps);
-
-  const { buttonProps } = useButton(
-    {
-      ...props,
-      isDisabled: props.disabled,
-    },
-    buttonRef
-  );
-
-  return (
-    <StyledButton {...variantProps} {...cssProps} {...buttonProps} ref={buttonRef} asChild={asChild}>
-      {children}
-    </StyledButton>
-  );
+export const Button = styled(rx.button, buttonRecipe, {
+  defaultProps: {
+    type: 'button',
+  },
 });
-
-Button.displayName = 'Button';
+Button.displayName = 'button';
 
 export default Button;
